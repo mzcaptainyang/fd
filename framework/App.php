0a44f17b1ea413b9fb444d494d4e4ef6e7532ec7
yf <yf@meitu.com>
2017-09-11 23:39:24 +0800

    初始化框架，一些小优化，开始构思如何组织基础配置文件更好

--- framework/App.php
+++ framework/App.php
@@ -9,8 +9,30 @@ class App
     const APP_MODE_WEB = 'web';
     const APP_MODE_CONSOLE = 'console';
 
+    private $init;
+    private $appMode = self::APP_MODE_WEB;
+    private $appName;
+    private $appPath;
+    private $appNamespace;
+    private $rootPath;
+
+    private function init($rootPath, $appName, $appMode = null)
+    {
+        $this->init = true;
+        $this->appName = $appName;
+        $this->appMode = $appMode;
+
+        $rootPath = rtrim($rootPath, '/\\');
+        $this->appPath = $rootPath . DS . 'src' . DS . 'App' . DS . $appName;
+        $this->rootPath = $rootPath;
+        $this->appNamespace = APP_NAMESPACE . DS . $this->appName;
+    }
+
     public function run($rootPath, $appName, $appMode = null)
     {
-
+        // 初始化框架
+        if (!$this->init) {
+            $this->init($rootPath, $appName, $appMode);
+        }
     }
 }
--- src/App/Api/public/index.php
+++ src/App/Api/public/index.php
@@ -4,10 +4,14 @@ $charset = 'UTF-8';
 ini_set('display_errors', 1);
 ini_set('default_charset', $charset);
 
-define('ROOT', dirname(dirname(dirname(dirname(dirname(__FILE__))))));
+define('DS', DIRECTORY_SEPARATOR);
+define('APP_PATH', dirname(__DIR__));
+define('SRC_PATH', dirname(dirname(APP_PATH)));
+define('ROOT', dirname(SRC_PATH));
+define('VENDOR_PATH', ROOT . DS . 'vendor');
 define('APP_NAMESPACE', '\\Work\\App\\' . 'Api');
 
-require ROOT . '/vendor/autoload.php';
+require VENDOR_PATH . DS . "autoload.php";
 
 $app = \Fd\App::getInstance();
 $app->run(ROOT, 'Api', \Fd\App::APP_MODE_WEB);
